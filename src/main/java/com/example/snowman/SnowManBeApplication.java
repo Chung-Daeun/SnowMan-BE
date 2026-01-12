6a53b0effb1c8e5c988835a0b3f0ab77c9bf5b44
Chung-Daeun <daeunchungb2m@gmail.com>
2026-01-12 17:05:02 +0900

    project init

---
+++ src/main/java/com/example/snowman/SnowManBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.snowman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowManBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowManBeApplication.class, args);
+    }
+
+}
