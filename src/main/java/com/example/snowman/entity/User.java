e40a1172314afa882709bdcf3c76c58feda72c5a
soohoioa <soohoioa@gmail.com>
2026-01-14 15:51:29 +0900

    OAuth2 구글 로그인

---
+++ src/main/java/com/example/snowman/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.snowman.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String googleSub;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    private User(String googleSub) {
+        this.googleSub = googleSub;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public static User create(String googleSub) {
+        return new User(googleSub);
+    }
+}
---
+++ src/main/java/com/example/snowman/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.snowman.repository;
+
+import com.example.snowman.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByGoogleSub(String googleSub);
+}
